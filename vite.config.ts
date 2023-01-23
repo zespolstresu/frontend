00c66135cbe9339f72f7390939171fab42411002
krisuv <1krzysztof.bujalski@gmail.com>
2023-01-23 15:10:21 +0100

    add dist build

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://spotus-backend.herokuapp.com/api',
+        target: 'https://spotus-backend.herokuapp.com/api',
         changeOrigin: true,
         secure: false,
         rewrite: path => path.replace('/api', '')
