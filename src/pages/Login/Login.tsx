cf4268f6dae376ec127399ac403dfc0b9c014a2a
krisuv <1krzysztof.bujalski@gmail.com>
2023-01-24 02:28:46 +0100

    change env variable

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -5,7 +5,6 @@ import CssBaseline from '@mui/material/CssBaseline';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
